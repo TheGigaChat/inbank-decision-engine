200c8d918e8e9b3aa7044a15e48b3063e05a7e19
Artur Dzekunov <artur.dzekunov10@gmail.com>
2026-03-26 10:53:29 +0200

    refactor: simplify profile resolution by using repository directly in decision service

--- backend/src/main/java/com/inbank/decision_engine/service/ProfileService.java
+++
@@ -1,7 +0,0 @@
-package com.inbank.decision_engine.service;
-
-import com.inbank.decision_engine.model.PersonProfile;
-
-public interface ProfileService {
-    PersonProfile resolveProfile(String personalCode);
-}
